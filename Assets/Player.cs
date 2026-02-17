eefbcc3f63a6ca6a10b8a015fdadc169025c496c
DESKTOP-L8PKNRH\ADMIN <nguyevanlong22072003@gmail.com>
2026-02-17 10:31:26 +0700

    Set up new input system (1)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,13 +6,32 @@ public class Player : MonoBehaviour
     public Player_IdleState idleState { get; private set; }
     public Player_MoveState moveState { get; private set; }
 
+    private PlayerInputSet inputActions;
+
     private void Awake()
     {
         stateMachine = new StateMachine();
+        inputActions = new PlayerInputSet();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Movement.performed += ctx => Debug.Log(ctx.ReadValue<Vector2>());
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
     }
 
     private void Start()
     {
-        stateMachine.Initialize(idleState);
+        //stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        
     }
 }
